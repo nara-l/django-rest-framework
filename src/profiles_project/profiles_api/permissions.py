dcc175322dc06e8d13c573e13e18ba7624ae67f2
Nara Lawrence <njielitumbe@gmail.com>
2018-05-15 19:59:56 -0400

    added login api

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -1,12 +1,11 @@
-# user permissions
 from rest_framework import permissions
 
 
 class UpdateOwnProfile(permissions.BasePermission):
-    '''Allow user to update their own profile'''
+    """Allow users to edit their own profile."""
 
     def has_object_permission(self, request, view, obj):
-        '''Check user is tryin gto edit their own profile'''
+        """Check user is trying to edit their own profile."""
 
         if request.method in permissions.SAFE_METHODS:
             return True
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -110,3 +112,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permissions_classes = (permissions.UpdateOwnProfile, )
     filters_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
